85d59b513663347b2ed11d3e3c8f15bd715d6c5a
polleya190-arch <polleya190@gmail.com>
2025-10-03 19:19:31 +0530

    added api on client

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL + "/api" });
 
 // ===== Salary =====
 export const addSalary = (data) => API.post("/salary", data);
